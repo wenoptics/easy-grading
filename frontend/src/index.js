22743422171b5352ece35cb3bbe522407079ee4c
Grayson Wen <wenoptics@163.com>
2018-12-10 17:21:38 -0500

    dynamic deduction works

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -11,8 +11,57 @@ $('form#login-form').on('submit', function(e) {
 
 });
 
-
 // grading.html
+// grading page, dynamic deductions
+var current_score = 100;
+var current_deduction_applied_list = [];
+var deduction_list = [
+	// ["0", "DO NOT RESUBMIT. YOU ARE DONE. YOUR GRADE HAS BEEN HUMAN VERIFIED BY A BACKREADER"],
+	["-15", "MINMAX METHOD DOES BRUTE FORCE WORK BY TESTING EACH VALUE AGAINST BOTH MIN AND MAX".toLowerCase()],
+	["-5", "ISPRIME USED UNNEEDED BOOLEAN VARIABLE(S). NONE ARE NEEDED".toLowerCase()],
+	["-5", "ISPRIME METHOD GOES BEYOND N/2 IN TESTING DIVISORS".toLowerCase()],
+	["+5", "ISPRIME METHOD STOPS TESTING FACTORS AT SQUARE ROOT OF N WHICH IS THE MINIMAL".toLowerCase()]
+	// ["-5", "ISPRIME METHOD SHOULD RETURN IMMEDIATELY UPON DETECTING A FACTOR. YOU ARE BEING INEFFICENT BY TESTING MORE FACTORS AFTER 1ST ONE IS DETECTED"],
+	// ["-5", "ISPERFECT USED UNNEEDED BOOLEAN VARIABLE(S). NONE ARE NEEDED"],
+	// ["-5", "ISPERFECT METHOD GOES BEYOND N/2 IN TESTING DIVISORS"],
+	// ["-5", "ISPERFECT METHOD USES AN IF ELSE TO RETURN THE BOOLEAN"]
+];
+
+function genLi(n, point, reason) {
+	var n = '<div class="form-check">'
+		+ '<input class="form-check-input grading-checkbox-deduction" type="checkbox" value="" id="defaultCheck' + n + '" data-points="'+point+'" >'
+		+ '<label class="form-check-label" for="defaultCheck' + n + '">'
+		+ '<code>' + point + '</code> ' + reason
+		+ '</label>'
+		+ '</div>';
+	return n;
+}
+
+// load all deductions on page load
+var deduction_parent = $('div#grading-deductions');
+deduction_parent.html("");
+for (var i=0; i<deduction_list.length; i++) {
+	var p = deduction_list[i][0];
+	var r = deduction_list[i][1];
+	deduction_parent.html(deduction_parent.html() + genLi(i, p, r));
+}
+
+function setOnChangeHandler() {
+	var nd = $('input.grading-checkbox-deduction');
+	nd.off('change');
+	nd.on('change', function (e) {
+		// console.log( $(this).attr('data-points') );
+		var sign = $(this).get(0).checked ? 1 : -1;
+		var addPoints = parseFloat($(this).attr('data-points'));
+
+		current_score += sign * addPoints;
+		console.log( 'current_score == ' + current_score);
+		$('#grading-score').html(current_score);
+	});
+}
+
+setOnChangeHandler();
+
 $('button#grading-add-deduction').on('click', function (e) {
 
 	e.preventDefault();
@@ -20,25 +69,15 @@ $('button#grading-add-deduction').on('click', function (e) {
 	var p = $('input#grading-points').val();
 	var r = $('input#grading-reason-text').val();
 
-	function genLi(n, str) {
-		return '<div class="form-check">'
-			+ '<input class="form-check-input" type="checkbox" value="" id="defaultCheck'+n+'">'
-			+ '<label class="form-check-label" for="defaultCheck'+n+'">'
-			+ str
-			+ '</label>'
-			+ '</div>'
-	}
-
-	var ck = $('div.form-check');
+	/*var ck = $('div.form-check');
 	ck = ck[ck.length - 1];
 
 	var i = $(ck).find('div input').id;
-	i = parseInt(i[i.length - 1]);
+	i = parseInt(i[i.length - 1]);*/
 
-	var parent = $('div#grading-deductions');
-	parent.html(parent.html() + genLi(i + 1, p + ' ' + r));
+	deduction_parent.html(deduction_parent.html() + genLi(deduction_list.length+1, p, r));
+	setOnChangeHandler();
 
-	return false;
 });
 
 // grading page, get user name from url
