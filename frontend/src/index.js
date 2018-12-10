1063c83d1fdb8111f7533188da21d758cdb80d33
Grayson Wen <wenoptics@163.com>
2018-12-10 00:46:54 -0500

    login logic

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,3 +1,26 @@
 import './index.scss'
 import 'bootstrap';
 
+// login.html
+$('form#login-form').on('submit', function(e) {
+
+	e.preventDefault();
+	window.location.href = ('dashboard.html?who=' + $("#input-login-email").val());
+
+});
+
+
+// grading.html
+$('button#grading-add-deduction').on('click', function (e) {
+
+	$('input#grading-points').val();
+	$('input#grading-reason-text').val();
+
+	'<div class="form-check">'
+	+	'<input class="form-check-input" type="checkbox" value="" id="defaultCheck4">'
+	+	'<label class="form-check-label" for="defaultCheck4">'
+	+	'FYI (no deduction) your text indentation sucks'
+	+'</label>'
+	+'</div>'
+
+});
